0e011d69c65094802e55731b687cb03916061e92
Igor <igor.shaposhnikov@urfu.me>
2024-03-11 16:52:35 +0500

    Подключил SignalR, Bootstrap, Radzen к Blazor Client

--- Football.Client/Program.cs
+++ Football.Client/Program.cs
@@ -1,11 +1,24 @@
 using Football.Client;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.SignalR.Client;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7116/api/") });
+
+builder.Services.AddSingleton(sp => {
+    var navigationManager = sp.GetRequiredService<NavigationManager>();
+    return new HubConnectionBuilder()
+      .WithUrl("https://localhost:7116/football-hub")
+      .WithAutomaticReconnect()
+      .Build();
+});
+
+builder.Services.AddRadzenComponents();
 
 await builder.Build().RunAsync();
